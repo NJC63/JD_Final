a304f221e5db9f165e8c247599fd80b282d9281e
Nathan Carozza <njc63@pitt.edu>
2024-04-14 19:39:52 -0400

    cleaning up

--- binary_search.py
+++ binary_search.py
@@ -1,27 +1,8 @@
-# def binarySearch_strings(array, x):
-#     i = 0
-#     length = len(array)
-
-#     while(i <= length):
-#         midpoint = i + ((length - 1) // 2)
-
-#         if(x == array[midpoint]):
-#             return midpoint
-        
-#         elif(x > array[midpoint]):
-#             i = midpoint + 1
-
-#         else:
-#             length = midpoint - 1
-#     return -1
-
-
 def binSearch(sorted_list, target):
     min = 0
     max = len(sorted_list) - 1
 
     while(min <= max):
-
         midpt = (min + max)//2
 
         if(sorted_list[midpt] == target):
