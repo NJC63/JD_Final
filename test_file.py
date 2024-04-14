61357bc1aa8a23236847de26ed5b04340b9ee553
Nathan Carozza <njc63@pitt.edu>
2024-04-14 18:44:39 -0400

    initial testing

---
+++ test_file.py
@@ -0,0 +1,7 @@
+
+
+
+# testing the sorted() function
+list = ["a", "c", "z", "p", "d", "b", "o", "i", "q", "t", "g", "s", "r", "u", "j", "k", "v", "e", "f", "w", "x", "n", "y", "l", "h", "m"]
+sorted_list = sorted(list)
+print(sorted_list)
