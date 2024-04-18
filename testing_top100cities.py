bd36dcd9ca20d0346d11657cc54a8e926f9079a2
Nathan Carozza <njc63@pitt.edu>
2024-04-18 14:13:23 -0400

    first iteration of if city is in top100 most populous cities

--- testing_top100cities.py
+++ testing_top100cities.py
@@ -13,10 +13,22 @@ row_for_city = df[df["city"] == city]
 
 population = row_for_city["population_2020"]
 
-if(df.isin([city]).any().any()):
+val = df["city"] == city
 
-    print("Your city is in the top 100 most populous cities in the United States.")
-    print("According to the 2020 census " + city + " has " + str(population[1]) + " people")
+i = 1
+
+while (i <= 100):
+    if(val[i] == True):
+        print(val[i])
+    i = i + 1
+
+
+
+
+# if(df.isin([city]).any().any()):
+
+#     print("Your city is in the top 100 most populous cities in the United States.")
+#     print("According to the 2020 census " + city + " has " + str(population[1]) + " people")
 
 
 
