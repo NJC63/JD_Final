ce5b71d1d1eb1f4552059b775e64bc304ecd248b
Nathan Carozza <njc63@pitt.edu>
2024-04-18 12:10:39 -0400

    more_top100citytesting

--- testing_top100cities.py
+++ testing_top100cities.py
@@ -1,24 +1,22 @@
 import pandas as pd
 
 
-df = pd.read_csv("top100cities.csv", header=0)
+df = pd.read_csv("top100cities.csv", index_col=0)
 df["city"] = df["city"].str.lower()
 df["state"] = df["state"].str.lower()
 
 
-print(df.head(5))
+city = input("Please enter your city:").lower()
 
-
-city = input("Please enter your city:")
 
 row_for_city = df[df["city"] == city]
-print(row_for_city)
-
 
 population = row_for_city["population_2020"]
 
-print("Your city is in the top 100 most populous cities in the United States.")
-print("According to the 2020 census " + city + " has " + str(population) + "people")
+if(df.isin([city]).any().any()):
+
+    print("Your city is in the top 100 most populous cities in the United States.")
+    print("According to the 2020 census " + city + " has " + str(population[1]) + " people")
 
 
 
