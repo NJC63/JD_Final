f4be15c5d549dd6a6eef47d5fb118fdeca32059d
Nathan Carozza <njc63@pitt.edu>
2024-04-20 19:10:59 -0400

    initial complete working program

--- functions.py
+++ functions.py
@@ -23,6 +23,40 @@ def clean(raw_df):
 
     return 1
 
+def cities(city):
+    df = pd.read_csv("cities.csv")
+
+    df["city"] = df["city"].str.lower()
+    df["st"] = df["st"].str.lower()
+    df["state"] = df["state"].str.lower()
+
+    row_for_city = df[df["city"] == city]
+
+    st = row_for_city["st"]
+    state = row_for_city["state"]
+    county = row_for_city["county"]
+    zip = row_for_city["zip"]
+
+    val = df["city"] == city
+    inCities = False
+    location = -1
+    i = 1
+
+    while (i <= 19501):
+        if(val[i] == True):
+            inCities = True
+            location = i
+        i += 1
+
+    if(inCities):
+        print(city.title() + " is in the state " + str(state[location]).title() + " with initials " + str(st[location]).upper() + ".")
+        print("County for " + city.title() + " is " + str(county[location]).title() + " County .")
+        print("Zip for " + city.title() + " is " + str(zip[location]) + ".")
+    else:
+        print("Your city could not be found.")
+
+
+
 
 def top100cities(city):
 
@@ -72,7 +106,7 @@ def airports(city):
     df["COUNTRY"] = df["COUNTRY"].str.lower()
 
     row_for_city = df[df["CITY"] == city]
-    
+
     airport = row_for_city["AIRPORT"]
     iata = row_for_city["IATA"]
 
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import functions as fn
 location = input("Please enter your desired city within the United States (city): ").lower()
 print()
 
+fn.cities(location)
+print()
 fn.top100cities(location)
 print()
 fn.airports(location)
